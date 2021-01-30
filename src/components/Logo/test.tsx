2d147bb70cd5c0c3d20a37da92539efe3e451c30
CriptX-42 <ricardo-svc@live.com>
2021-01-30 18:00:30 -0300

    Fazendo a logo para componentes mobile

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -1,39 +1,46 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
+import 'jest-styled-components'
 
 import Logo from '.'
 
 describe('<Logo />', () => {
   it('should render a white label by default', () => {
-    //? Renderizar o componente `render`
-
-    //? Selecionar o elemento a testar `screen`
-
-    //? expect - assertion - asserção - comparação - analise (espero que renderize a logo branca)
-    renderWithTheme(<Logo/>)
-    expect(screen.getByLabelText(/Won Games/i).parentElement)
-    .toHaveStyle({color:'#FAFAFA'})
+    renderWithTheme(<Logo />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: '#FAFAFA'
+    })
   })
 
-  it('should render a black label when colors is passed', () => {
-
-    renderWithTheme(<Logo  color="black" />)
+  it('should render a black label when color is passed', () => {
+    renderWithTheme(<Logo color="black" />)
     expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
       color: '#030517'
     })
   })
 
   it('should render a normal logo when size is default', () => {
-    renderWithTheme(<Logo  />)
+    renderWithTheme(<Logo />)
     expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
       width: '11rem'
     })
   })
 
-  it('should render a big logo', () => {
+  it('should render a bigger logo', () => {
     renderWithTheme(<Logo size="large" />)
     expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
       width: '20rem'
     })
+  })
+
+  it('should render a bigger logo without text if hideOnMobile', () => {
+    renderWithTheme(<Logo hideOnMobile />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyleRule(
+      'width',
+      '5.8rem',
+      {
+        media: '(max-width: 768px)'
+      }
+    )
   })
 })
