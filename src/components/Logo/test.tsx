4eb50f4bde73692552bcb3257a967c585f6abfe1
CriptX-42 <ricardo-svc@live.com>
2021-01-30 15:44:42 -0300

    Criando um helper para componentes que necessitam de temas nos testes

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -3,11 +3,11 @@ import { render, screen } from '@testing-library/react'
 import Logo from '.'
 
 describe('<Logo />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Logo />)
+  it('should render a white label by default', () => {
+    //? Renderizar o componente `render`
 
-    expect(screen.getByRole('heading', { name: /Logo/i })).toBeInTheDocument()
+    //? Selecionar o elemento a testar `screen`
 
-    expect(container.firstChild).toMatchSnapshot()
+    //? expect - assertion - asserção - comparação - analise (espero que renderize a logo branca)
   })
 })
