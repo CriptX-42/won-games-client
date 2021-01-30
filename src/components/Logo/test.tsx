3c6b6e159747537b197ccb40130bdc49d2d6b79d
CriptX-42 <ricardo-svc@live.com>
2021-01-30 16:15:19 -0300

    Fazendo os testes da logo

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import Logo from '.'
 
@@ -9,5 +10,16 @@ describe('<Logo />', () => {
     //? Selecionar o elemento a testar `screen`
 
     //? expect - assertion - asserção - comparação - analise (espero que renderize a logo branca)
+    renderWithTheme(<Logo/>)
+    expect(screen.getByLabelText(/Won Games/i).parentElement)
+    .toHaveStyle({color:'#FAFAFA'})
+  })
+
+  it('should render a black label when colors is passed', () => {
+
+    renderWithTheme(<Logo  color="black" />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: '#030517'
+    })
   })
 })
