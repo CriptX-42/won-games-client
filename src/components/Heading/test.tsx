88517e4cfd75090d389965cad6b04237efa490b5
CriptX-42 <ricardo-svc@live.com>
2021-01-31 16:48:53 -0300

    Criando o 'mediaMatch' e resolvendo os testes

--- src/components/Heading/test.tsx
+++ src/components/Heading/test.tsx
@@ -8,28 +8,30 @@ describe('<Heading />', () => {
   it('should render a white heading by default', () => {
     renderWithTheme(<Heading>Won Games</Heading>)
     expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
-      color: '#030517'
+      color: '#FAFAFA'
     })
   })
 
-  it('should render a black heading', () => {
-    renderWithTheme(<Heading color='white'>Won Games</Heading>)
+  it('should render a black heading when color is passed', () => {
+    renderWithTheme(<Heading color="black">Won Games</Heading>)
     expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
-      color: '#FAFAFA'
+      color: '#030517'
     })
   })
 
   it('should render a heading with a line to the left side', () => {
     renderWithTheme(<Heading lineLeft>Won Games</Heading>)
     expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
-      'border-left': '0.7rem solid ${theme.colors.#3CD3C1}'
+      'border-left': '0.7rem solid #3CD3C1'
     })
   })
 
   it('should render a heading with a line at the bottom', () => {
     renderWithTheme(<Heading lineBottom>Won Games</Heading>)
     expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyleRule(
-      'border-bottom', '0.4rem solid #F231A5', {
+      'border-bottom',
+      '0.5rem solid #F231A5',
+      {
         modifier: '::after'
       }
     )
---
+++ src/components/MediaMatch/index.tsx
@@ -0,0 +1,25 @@
+import styled, { css } from 'styled-components'
+import media, { DefaultBreakpoints } from "styled-media-query";
+
+type breackPoint = keyof DefaultBreakpoints
+export type MediaMatchProps = {
+  lessThan?: breackPoint
+  greaterThan?: breackPoint
+}
+
+const mediaMatchModifiers = {
+  lassThan: (size: breackPoint) => css`
+    ${media.lessThan(size)`display: block`}
+  `,
+  greaterThan: (size: breackPoint) => css`
+    ${media.greaterThan(size)`display: block`}
+  `
+}
+
+export default styled.div<MediaMatchProps>`
+  ${({ lessThan, greaterThan }) => css`
+    display: none;
+    ${!!lessThan && mediaMatchModifiers.lassThan(lessThan)}
+    ${!!greaterThan && mediaMatchModifiers.greaterThan(greaterThan)}
+  `}
+`
