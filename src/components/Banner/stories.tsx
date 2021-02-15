46234a790ad031da4326598d633b70a47fdf9b20
CriptX-42 <ricardo-svc@live.com>
2021-02-14 22:02:11 -0300

    Fazendo o banner do projeto com os testes

--- src/components/Banner/stories.tsx
+++ src/components/Banner/stories.tsx
@@ -3,7 +3,14 @@ import Banner from '.'
 
 export default {
   title: 'Banner',
-  component: Banner
+  component: Banner,
+  args: {
+    img: 'https://source.unsplash.com/user/willianjusten/1042x580',
+    title: 'Defy death',
+    subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+    buttonLabel: 'Buy now',
+    buttonLink: '/games/defy-death'
+  }
 } as Meta
 
-export const Default: Story = () => <Banner />
+export const Default: Story<BannerProps> = (args) => <Banner {...args} />
