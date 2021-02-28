04475eea48942c86ea5adc162023d5e298653dc0
CriptX-42 <ricardo-svc@live.com>
2021-02-27 22:17:58 -0300

    Iniciando inserção do Ribbon no banner (fazendo primeiro tdd)

--- src/components/Banner/test.tsx
+++ src/components/Banner/test.tsx
@@ -1,7 +1,6 @@
 import { render, screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
 
-
 import Banner from '.'
 
 const props = {
@@ -23,10 +22,26 @@ describe('<Banner />', () => {
       screen.getByRole('heading', { name: /Play the new CrashLands season/i })
     ).toBeInTheDocument()
 
-    expect(
-      screen.getByRole('img', { name: /Defy death/i })
-    ).toBeInTheDocument()
+    expect(screen.getByRole('img', { name: /Defy death/i })).toBeInTheDocument()
 
     expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('should render a Ribbon', () => {
+    renderWithTheme(
+      <Banner
+        {...props}
+        ribbon="My Ribbon"
+        ribbonSize="small"
+        ribbonColor="secondary"
+      />
+    )
+    const ribbon = screen.getByText(/My Ribbon/i)
+
+    expect(ribbon).toBeInTheDocument()
+    expect(ribbon).toHaveStyle({
+      backgroundColor: '#3CD3C1'
+    })
+    expect(ribbon).toHaveStyle({ height: '3.6rem', fontSize: '1.4rem' })
   })
 })
