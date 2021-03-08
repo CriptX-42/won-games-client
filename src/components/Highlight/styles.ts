d5e09c83ac0069814d7298be1f2446ab2f8fa19a
CriptX-42 <ricardo-svc@live.com>
2021-03-07 23:23:32 -0300

    Iniciando construção do css para 'Highlight'

--- src/components/Highlight/styles.ts
+++ src/components/Highlight/styles.ts
@@ -1,5 +1,40 @@
-import styled from 'styled-components'
+import styled, {css} from 'styled-components'
 
-export const Wrapper = styled.main``
-export const Title = styled.h2``
-export const Subtitle = styled.h3``
+export const Wrapper = styled.main`
+  ${({ theme }) => css`
+    position: relative;
+    height: 23rem;
+    display:grid;
+
+    &::after{
+      content:'';
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      background-color: rgba(0, 0, 0, 0.6);
+    }
+  `}
+`
+export const Content = styled.div`
+  ${({ theme }) => css`
+    z-index: ${theme.layers.base};
+    text-align: right;
+    padding: ${theme.spacings.xsmall};
+  `}
+`
+
+export const Title = styled.h2`
+  ${({ theme }) => css`
+    font-size: ${theme.font.sizes.large};
+    font-weight: ${theme.font.bold};
+    color: ${theme.colors.white};
+  `}
+`
+export const Subtitle = styled.h3`
+  ${({ theme }) => css`
+    font-size: ${theme.font.sizes.small};
+    font-weight: ${theme.font.light};
+    color: ${theme.colors.white};
+    margin-bottom: ${theme.spacings.medium};
+  `}
+`
