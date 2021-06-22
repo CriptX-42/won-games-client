306565e1c17b6220b92f14a0091e79b97ef53dc1
CriptX-42 <ricardo-svc@live.com>
2021-06-21 21:55:25 -0300

    Criando a estrutura do GameCard. WIP testes unitários

--- src/components/GameCard/stories.tsx
+++ src/components/GameCard/stories.tsx
@@ -1,9 +1,19 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
-import GameCard from '.'
+import GameCard, { GameCardProps } from '.'
 
 export default {
   title: 'GameCard',
-  component: GameCard
+  component: GameCard,
+  args: {
+    title: 'Skyrim',
+    developer: 'Bethesda',
+    img: 'img',
+    price: 'R$ 200,00'
+  }
 } as Meta
 
-export const Default: Story = () => <GameCard />
+export const Default: Story<GameCardProps> = (args) => (
+  <div style={{ width: '30rem' }}>
+    <GameCard {...args} />
+  </div>
+)
