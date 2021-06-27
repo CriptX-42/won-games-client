307f830dd579e426d56bcb7dc659626c56dc1f0e
CriptX-42 <ricardo-svc@live.com>
2021-06-26 21:51:27 -0300

    Criando teste e estilo de gameCard

--- src/components/GameCard/test.tsx
+++ src/components/GameCard/test.tsx
@@ -2,12 +2,26 @@ import { render, screen } from '@testing-library/react'
 
 import GameCard from '.'
 
+const props = {
+  title: 'Population Zero',
+  developer: 'Rockstar Games',
+  img: 'https://source.unsplash.com/user/willianjusten/300x140',
+  price: 'R$ 235,00'
+}
+
 describe('<GameCard />', () => {
   it('should render the heading', () => {
-    const { container } = render(<GameCard />)
-
-    expect(screen.getByRole('heading', { name: /GameCard/i })).toBeInTheDocument()
-
-    expect(container.firstChild).toMatchSnapshot()
+    expect(
+      screen.getByRole('heading', { name: props.title })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: props.developer })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: props.price })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole(screen.getByLabelText(/add to wishlist/i))
+    ).toBeInTheDocument()
   })
 })
