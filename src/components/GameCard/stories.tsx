f0f5a9e284372e2d0c49d32d182a40efa9cd17e5
CriptX-42 <ricardo-svc@live.com>
2021-07-11 19:52:10 -0300

    Colocando Ribbon no GameCard

--- src/components/GameCard/stories.tsx
+++ src/components/GameCard/stories.tsx
@@ -1,4 +1,5 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
+
 import GameCard, { GameCardProps } from '.'
 
 export default {
@@ -8,10 +9,12 @@ export default {
     title: 'Population Zero',
     developer: 'Rockstar Games',
     img: 'https://source.unsplash.com/user/willianjusten/300x140',
-    price: 'R$ 235,00'
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00'
   },
   argTypes: {
-    onFav: { action: 'clicked' }
+    onFav: { action: 'clicked' },
+    ribbon: { type: 'string' }
   }
 } as Meta
 
@@ -20,3 +23,15 @@ export const Default: Story<GameCardProps> = (args) => (
     <GameCard {...args} />
   </div>
 )
+
+export const WithRibbon: Story<GameCardProps> = (args) => (
+  <div style={{ width: '30rem' }}>
+    <GameCard {...args} />
+  </div>
+)
+
+WithRibbon.args = {
+  ribbon: '20% OFF',
+  ribbonSize: 'small',
+  ribbonColor: 'primary'
+}
