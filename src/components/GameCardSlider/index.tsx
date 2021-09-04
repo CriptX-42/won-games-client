bcec47eda43ee651624105f66d3efc05d36d51c5
CriptX-42 <ricardo-svc@live.com>
2021-09-04 16:18:10 -0300

    Aplicando o slider e gameCard ao 'GameCardSlider'

--- src/components/GameCardSlider/index.tsx
+++ src/components/GameCardSlider/index.tsx
@@ -1,8 +1,20 @@
 import * as S from './styles'
+import GameCard, { GameCardProps } from 'components/GameCard'
+import Slider, { SliderSettings } from 'components/Slider'
 
-const GameCardSlider = () => (
-  <S.Wrapper>
-    <h1>GameCardSlider</h1>
+type GameCardSliderProps = {
+  items: GameCardProps[]
+  color?: 'white' | 'black'
+}
+const settings: SliderSettings = {}
+
+const GameCardSlider = ({ items, color = 'black' }: GameCardSliderProps) => (
+  <S.Wrapper color={color}>
+    <Slider settings={settings}>
+      {items.map((item, index) => (
+        <GameCard key={index} {...item} />
+      ))}
+    </Slider>
   </S.Wrapper>
 )
 
