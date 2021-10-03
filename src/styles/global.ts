c5a4b0f60c6d0e9fbcde9275e866ab4251843cff
CriptX-42 <ricardo-svc@live.com>
2021-10-02 21:35:24 -0300

    Colocando box-sizing nos pseudos seletores 'before' e 'after' de forma global

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -42,6 +42,11 @@ const GlobalStyles = createGlobalStyle`
     box-sizing: border-box;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
+
+    &::before,
+    &::after {
+      box-sizing: inherit;
+    }
   }
 
   ${({ theme }) => css`
@@ -58,6 +63,7 @@ const GlobalStyles = createGlobalStyle`
     body {
       font-family: ${theme.font.family};
       font-size: ${theme.font.sizes.medium};
+      background-color: ${theme.colors.mainBg};
     }
   `}
 
