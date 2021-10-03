130335921bae0c7f2c3dfdaf2ffb1938eb141e5f
CriptX-42 <ricardo-svc@live.com>
2021-10-02 21:06:43 -0300

    Removendo arquivos que não vamos utilizar no 'container' (dessa vez os arquivos certos, desculpe estou meio sonolento)

--- src/components/Container/test.tsx
+++ src/components/Container/test.tsx
@@ -6,8 +6,6 @@ describe('<Container />', () => {
   it('should render the heading', () => {
     const { container } = render(<Container />)
 
-    expect(screen.getByRole('heading', { name: /Container/i })).toBeInTheDocument()
-
     expect(container.firstChild).toMatchSnapshot()
   })
 })
