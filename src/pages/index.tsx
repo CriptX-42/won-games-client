a554052afdc63ee1175e00bee3eef50a1d3a56c9
CriptX-42 <ricardo-svc@live.com>
2022-03-22 20:43:30 -0300

    feat: :art: Conectando os dados da página

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,29 @@
-import Home from 'templates/Home'
-export default function Index() {
-  return <Home />
+import Home, { HomeTemplateProps } from 'templates/Home'
+import bannerMock from 'components/BannerSlider/mock'
+import gamesMock from 'components/GameCardSlider/mock'
+import highlightMock from 'components/Highlight/mock'
+
+export default function Index(props: HomeTemplateProps) {
+  return <Home {...props} />
+}
+
+/*
+  ? getStaticProps => Gera estático em build time
+  ? getServerSideProps => gerar via ssr a cada request
+  ? getInitialProps => gerar via ssr a cada request
+*/
+export function getServerSideProps() {
+  return {
+    props: {
+      banners: bannerMock,
+      newGames: gamesMock,
+      mostPopularHighlight: highlightMock,
+      mostPopularGames: gamesMock,
+      upcommingGames: gamesMock,
+      upcommingHighligth: highlightMock,
+      upcommingMoreGames: gamesMock,
+      freeGames: gamesMock,
+      freeHighligth: highlightMock
+    }
+  }
 }
