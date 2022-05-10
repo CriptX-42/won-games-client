bb374fc5236f71dc698b96e94c4a2b14c2d79102
CriptX-42 <ricardo-svc@live.com>
2022-05-09 23:00:48 -0300

    ✨ adicionando método onChenge

--- src/components/Checkbox/test.tsx
+++ src/components/Checkbox/test.tsx
@@ -1,6 +1,7 @@
-import { screen } from '@testing-library/react'
+import { screen, waitFor } from '@testing-library/react'
 import theme from 'styles/theme'
 import { renderWithTheme } from 'utils/tests/helpers'
+import userEvent from '@testing-library/user-event'
 
 import Checkbox from '.'
 
@@ -31,6 +32,18 @@ describe('<Checkbox />', () => {
 
     expect(screen.getByText(/checkbox label/i)).not.toHaveStyle({
       color: theme.colors.black
+    })
+  })
+
+  it('should dispatch onCheck when status chenges', async () => {
+    const onCheck = jest.fn()
+
+    renderWithTheme(<Checkbox label="checkbox" onCheck={onCheck} />)
+
+    expect(onCheck).not.toHaveBeenCalled()
+    userEvent.click(screen.getByRole('checkbox'))
+    await waitFor(() => {
+      expect(onCheck).toHaveBeenCalledTimes(1)
     })
   })
 })
