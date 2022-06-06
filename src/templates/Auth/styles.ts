742dd2eae8794741e281b3f7869a07cf74c27c4f
CriptX-42 <ricardo-svc@live.com>
2022-06-05 22:59:20 -0300

    🚧 - Criando estrutura inicial do template de sign in e sign up

--- src/templates/Auth/styles.ts
+++ src/templates/Auth/styles.ts
@@ -1,3 +1,11 @@
 import styled from 'styled-components'
 
 export const Wrapper = styled.main``
+
+export const BannerBlock = styled.div``
+
+export const Subtitle = styled.div``
+
+export const Footer = styled.p``
+
+export const Content = styled.div``
