46dbd38e028e1ae05972929568c2271a14930173
CriptX-42 <ricardo-svc@live.com>
2022-07-11 23:19:13 -0300

    ✨ - Testado formSigIn

--- src/components/FormSignIn/test.tsx
+++ src/components/FormSignIn/test.tsx
@@ -1,15 +1,35 @@
 import { render, screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import FormSignIn from '.'
 
 describe('<FormSignIn />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<FormSignIn />)
+  it('should render the form', () => {
+
+    renderWithTheme(<FormSignIn />)
+
+    expect(screen.getByPlaceholderText(/email/i)).toBeInTheDocument()
+    expect(screen.getByPlaceholderText(/password/i)).toBeInTheDocument()
+    expect(
+      screen.getByRole('button', { name: /sign in now/i })
+    ).toBeInTheDocument()
+  })
+
+  it('should render the forgot password link', () => {
+    renderWithTheme(<FormSignIn />)
 
     expect(
-      screen.getByRole('heading', { name: /FormSignIn/i })
+      screen.getByRole('link', { name: /forgot your password\?/i })
     ).toBeInTheDocument()
-
-    expect(container.firstChild).toMatchSnapshot()
   })
-})
+
+  it('should render the forgot password link', () => {})
+  it('should render text to sign up if already have an account', () => {
+    renderWithTheme(<FormSignIn />)
+
+  it('should render the text and link to sign up', () => {
+
+    expect(screen.getByRole('link', { name: /sign up/i })).toBeInTheDocument()
+    expect(screen.getByText(/don’t have an account\?/i)).toBeInTheDocument()
+  })
+});
