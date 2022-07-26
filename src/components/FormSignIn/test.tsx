409b5f1157bf4620a018fc21a7a8a7845eb9bca7
CriptX-42 <ricardo-svc@live.com>
2022-07-25 23:09:18 -0300

    ✨ - Fazendo estilo do formSignIn

--- src/components/FormSignIn/test.tsx
+++ src/components/FormSignIn/test.tsx
@@ -5,14 +5,15 @@ import FormSignIn from '.'
 
 describe('<FormSignIn />', () => {
   it('should render the form', () => {
-
-    renderWithTheme(<FormSignIn />)
+    const { container } = renderWithTheme(<FormSignIn />)
 
     expect(screen.getByPlaceholderText(/email/i)).toBeInTheDocument()
     expect(screen.getByPlaceholderText(/password/i)).toBeInTheDocument()
     expect(
       screen.getByRole('button', { name: /sign in now/i })
     ).toBeInTheDocument()
+
+    expect(container.parentElement).toMatchSnapshot()
   })
 
   it('should render the forgot password link', () => {
@@ -23,13 +24,10 @@ describe('<FormSignIn />', () => {
     ).toBeInTheDocument()
   })
 
-  it('should render the forgot password link', () => {})
   it('should render text to sign up if already have an account', () => {
     renderWithTheme(<FormSignIn />)
-
-  it('should render the text and link to sign up', () => {
 
     expect(screen.getByRole('link', { name: /sign up/i })).toBeInTheDocument()
     expect(screen.getByText(/don’t have an account\?/i)).toBeInTheDocument()
   })
-});
+})
