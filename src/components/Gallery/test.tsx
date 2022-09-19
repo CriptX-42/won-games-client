9c7648379ec338fe9d028365fe596685d3d3380e
CriptX-42 <ricardo-svc@live.com>
2022-09-19 20:00:34 -0300

    ✨ - CCriando o slider das thumbnails

--- src/components/Gallery/test.tsx
+++ src/components/Gallery/test.tsx
@@ -6,7 +6,9 @@ describe('<Gallery />', () => {
   it('should render the heading', () => {
     const { container } = render(<Gallery />)
 
-    expect(screen.getByRole('heading', { name: /Gallery/i })).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /Gallery/i })
+    ).toBeInTheDocument()
 
     expect(container.firstChild).toMatchSnapshot()
   })
