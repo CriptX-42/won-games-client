771645d86e5711917eb6e0aec9f8904b770eec94
CriptX-42 <ricardo-svc@live.com>
2023-02-26 13:16:51 -0300

    feat(carddropdown): 🎨 - Corrigindo teste

--- src/components/CartDropdown/test.tsx
+++ src/components/CartDropdown/test.tsx
@@ -1,7 +1,7 @@
 import { screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
 
-import items from 'components/CartList/mock'
+import items from 'components/CardList/mock'
 
 import CartDropdown from '.'
 
