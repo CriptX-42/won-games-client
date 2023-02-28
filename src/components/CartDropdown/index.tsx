f0b1df55ed926b1db22d0e255de69b0d331fa929
CriptX-42 <ricardo-svc@live.com>
2023-02-27 21:59:13 -0300

    feat(carddropdown): 🎨 - Criando dropdown vazio

--- src/components/CartDropdown/index.tsx
+++ src/components/CartDropdown/index.tsx
@@ -5,13 +5,13 @@ import CartIcon from 'components/CartIcon'
 import CartList from 'components/CardList'
 
 export type CartDropdownProps = {
-  items: GameItemProps[]
-  total: string
+  items?: GameItemProps[]
+  total?: string
 }
 
 const CartDropdown = ({ items, total }: CartDropdownProps) => (
   <S.Wrapper>
-    <Dropdown title={<CartIcon quantity={items.length} />}>
+    <Dropdown title={<CartIcon quantity={items?.length} />}>
       <CartList items={items} total={total}></CartList>
     </Dropdown>
   </S.Wrapper>
