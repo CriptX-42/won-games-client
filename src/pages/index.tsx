361e513d9ace66e3d4fcc32cddf969765075271b
CriptX-42 <ricardo-svc@live.com>
2023-04-27 19:19:49 -0300

    feat(peckage): ✨ - Instalando apollo e adicionando a index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,52 @@
+import { ApolloClient, gql, InMemoryCache } from '@apollo/client'
+
 import Home, { HomeTemplateProps } from 'templates/Home'
-import bannerMock from 'components/BannerSlider/mock'
+import bannersMock from 'components/BannerSlider/mock'
 import gamesMock from 'components/GameCardSlider/mock'
 import highlightMock from 'components/Highlight/mock'
 
 export default function Index(props: HomeTemplateProps) {
+  const client = new ApolloClient({
+    uri: 'http://localhost:1337/graphql',
+    cache: new InMemoryCache()
+  })
+
+  client.query({
+    query: gql`
+      query getGames {
+        games {
+          name
+        }
+      }
+    `
+  })
+
   return <Home {...props} />
 }
 
-/*
-  ? getStaticProps => Gera estático em build time
-  ? getServerSideProps => gerar via ssr a cada request
-  ? getInitialProps => gerar via ssr a cada request
-*/
+// ATENÇÃO:
+// os métodos getStaticProps/getServerSideProps SÓ FUNCIONAM EM PAGES
+
+// getStaticProps => gerar estático em build time
+// getServerSideProps => gerar via ssr a cada request
+// getInitialProps => gerar via ssr a cada request
 export function getServerSideProps() {
+  // faz lógica
+  // pode ser buscar dados numa API
+  // fazer calculo|leitura de context
+
+  // retorno dos dados
   return {
     props: {
-      banners: bannerMock,
+      banners: bannersMock,
       newGames: gamesMock,
       mostPopularHighlight: highlightMock,
       mostPopularGames: gamesMock,
-      upcommingGames: gamesMock,
-      upcommingHighligth: highlightMock,
-      upcommingMoreGames: gamesMock,
+      upcomingGames: gamesMock,
+      upcomingHighlight: highlightMock,
+      upcomingMoreGames: gamesMock,
       freeGames: gamesMock,
-      freeHighligth: highlightMock
+      freeHighlight: highlightMock
     }
   }
 }
