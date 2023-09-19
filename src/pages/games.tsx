06be5ee5d81fa2a310cdf88da889c2ab3304831d
CriptX-42 <ricardo-svc@live.com>
2023-09-18 23:38:50 -0300

    refactor(Games): 💥 - Criando limites para amostragem de pagina games

--- src/pages/games.tsx
+++ src/pages/games.tsx
@@ -12,7 +12,8 @@ export async function getStaticProps() {
   const apolloClient = initializeApollo()
 
   const { data } = await apolloClient.query({
-    query: QUERY_GAMES
+    query: QUERY_GAMES,
+    variables: { limit: 9 }
   })
 
   return {
@@ -21,7 +22,7 @@ export async function getStaticProps() {
       games: data.games.map((game: any) => ({
         title: game.name,
         developer: game.developers[0].name,
-        img: `http://localhost:1337${game.cover.url}`,
+        img: `http://localhost:1337${game.cover?.url}`,
         price: new Intl.NumberFormat('en', {
           style: 'currency',
           currency: 'USD'
